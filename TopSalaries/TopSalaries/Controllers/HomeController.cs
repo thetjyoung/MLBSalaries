c868eaf923a8f98212781af3584454f41a2c517e
TJ Young <thetjyoung@gmail.com>
2013-04-17 13:58:38 -0600

    Completed Web Service Call

--- TopSalaries/TopSalaries/Controllers/HomeController.cs
+++ TopSalaries/TopSalaries/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Net;
 using System.Xml;
+using System.Xml.Linq;
+using System.Web.Script.Serialization;
 
 namespace TopSalaries.Controllers
 {
@@ -20,16 +22,38 @@ namespace TopSalaries.Controllers
 
         public ActionResult CallWebService(int? Year)
         {
-            string locationsRequest = CreateRequest(Convert.ToString(Year).Trim());
-            XmlDocument locationsResponse = MakeRequest(locationsRequest);
-            //process response
-            return View();
+            string salariesRequest = CreateRequest(Convert.ToString(Year).Trim());
+            XmlDocument salariesResponse = MakeRequest(salariesRequest);
+            string[][] salaries = ProcessResponse(salariesResponse);
+            JavaScriptSerializer serial = new JavaScriptSerializer();
+            var output = serial.Serialize(salaries);
+            return Json(output);
+        }
+
+        private string[][] ProcessResponse(XmlDocument salariesResponse)
+        {
+            string[][] salaries = new string[10][];
+            XmlNodeList nodes = salariesResponse.SelectNodes("root/salaries/salary");
+            int index = 0;
+            foreach (XmlNode node in nodes)
+            {
+                string[] tmp = new string[]{
+                    node.Attributes["rank"].Value.ToString(),
+                    node.Attributes["player"].Value.ToString(),
+                    node.Attributes["position_desc"].Value.ToString(),
+                    node.Attributes["salary"].Value.ToString()
+                };
+                salaries[index] = tmp;
+                index++;
+            }
+            return salaries;
         }
 
         public static string CreateRequest(string year)
         {
             string UrlRequest = "http://api.usatoday.com/open/salaries/mlb?"
-                                + " year=" + year.Trim()
+                                + "players="
+                                + "&seasons=" + year
                                 + "&top=10"
                                 + "&api_key=tpm3v8dne2t6g6km4x7tcwdn";
             return (UrlRequest);
